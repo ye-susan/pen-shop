eb6244078684bdbcf23338e1662b2c18db88afdc
Susan Ye <susanye3@gmail.com>
2020-07-10 13:50:47 -0700

    added and styled menu and homepage components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 
 function App() {
   return (
     <div>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet"/> 
       <HomePage />
     </div>
   );
