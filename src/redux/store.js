5c6f604998adc1ef581fbd374245b21908541cab
ye-susan <susanye3@gmail.com>
2021-04-08 11:42:45 -0700

    add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import rootReduceer from './root-reducer';
+
+const middlewares = [
+  logger
+];
+
+const store = createStore(rootReduceer, applyMiddleware(...middlewares))
+
+export default store;
