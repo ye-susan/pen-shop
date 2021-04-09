f9a2a492cd5bd075fa913b87b892fd9964550419
ye-susan <susanye3@gmail.com>
2021-04-09 15:46:28 -0700

    added cart selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,16 @@
+import { createSelector } from "reselect";
+
+//input selector, pull pieces of data from state
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems => 
+    cartItems.reduce (
+      (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0) 
+);
